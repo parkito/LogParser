ba57d3ff37e2940c65042b821954c5261b0fc8e4
parkito <artyom-karnov@yandex.ru>
2019-05-11 21:37:54 +0300

    Tune dispatch plan

--- src/main/kotlin/ru/siksmfp/kotlin/log/parser/DispatchPlan.kt
+++ src/main/kotlin/ru/siksmfp/kotlin/log/parser/DispatchPlan.kt
@@ -3,18 +3,18 @@ package ru.siksmfp.kotlin.log.parser
 import ru.siksmfp.kotlin.log.parser.dispatch.api.Dispatcher
 import java.io.BufferedReader
 import java.io.RandomAccessFile
+import java.nio.channels.FileChannel
 import java.nio.file.Files
 import java.nio.file.Paths
 
 class DispatchPlan(
-        private val logFilePath: String,
-        private val outputPath: String,
-        private val dispatchers: List<Dispatcher>) {
+        logFilePath: String,
+        outputPath: String,
+        dispatchers: List<Dispatcher>) {
 
-    private val dispatchIterator = DispatchIterator(dispatchers)
-    private val chanel = RandomAccessFile(outputPath, "rw").channel
-    private val bufReader = BufferedReader(Files.newBufferedReader(Paths.get(logFilePath)))
-    private var dispatcher = dispatchIterator.getFirst()
+    private val dispatchIterator: DispatchIterator = DispatchIterator(dispatchers)
+    private val chanel: FileChannel = RandomAccessFile(outputPath, "rw").channel
+    private val bufReader: BufferedReader = BufferedReader(Files.newBufferedReader(Paths.get(logFilePath)))
 
     fun executePlan() {
         while (bufReader.ready()) {
